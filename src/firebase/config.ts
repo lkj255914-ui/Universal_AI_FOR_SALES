3cb98ad784a4dea78e8742fba11b4292c7ef9c16
Hsjs Bdj <jpratap731@gmail.com>
2025-11-24 08:11:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-502824506-6d5e9",
+  "appId": "1:137707885924:web:da6fb34266a874cdd45671",
+  "apiKey": "AIzaSyCD37rg6FfxvEUmXjXZwuc2SGWaf5R9B08",
+  "authDomain": "studio-502824506-6d5e9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "137707885924"
+};
